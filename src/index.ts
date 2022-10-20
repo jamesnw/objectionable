5d36e0a067ea6d4d6f066b4204cbba0fcce2de4d
James Stuckey Weber <jamesnw@gmail.com>
2022-10-19 20:42:58 -0400

    Set up options and default callbacks, prettier

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,51 @@
 interface GenericObserved {
-    [key: string | symbol]: any;
-} 
-export default function(observed: GenericObserved, path: string ='') : GenericObserved{
-    function pathedHandler (path: string) : ProxyHandler<GenericObserved>{
-        return {
-            set(obj: typeof observed, prop: keyof typeof observed, value: any){
-                obj[prop] = value;
-                console.log(`Set: ${path}/${String(prop)} to ${value}`)
-                return true;
-            }
+  [key: string | symbol | number]: any;
+}
+
+type iObjectReporterCallback = (
+  object: any,
+  prop: string | symbol,
+  path: string,
+  value: any
+) => void;
+interface iObjectOptions {
+  setValue?: boolean;
+  reporter?: iObjectReporterCallback;
+}
+
+export default function (
+  observed: GenericObserved,
+  { setValue = true, reporter }: iObjectOptions = {}
+): GenericObserved {
+  let defaultReporter: iObjectReporterCallback = function (
+    object,
+    prop,
+    path,
+    value
+  ) {
+    console.log(`Set: ${path} to ${value}`);
+  };
+  const reporterToUse = reporter ? reporter : defaultReporter;
+  function pathedHandler(path: string = ""): ProxyHandler<GenericObserved> {
+    return {
+      set(obj: typeof observed, prop: keyof typeof observed, value: any) {
+        if (setValue) {
+          obj[prop] = value;
         }
-    }
-    function observe(item: GenericObserved, hand: object, path: string){
-        const keys = Object.keys(item);
-        keys.forEach(key=>{
-            if(typeof item[key] === 'object'){
-                item[key] = observe(item[key], hand, path + key);
-            }
-        })
-        return new Proxy(item, pathedHandler(path))
-        
-    }
-    return observe(observed, pathedHandler(path), path);
+        const fullPath = `${path}/${String(prop)}`;
+        reporterToUse(observed, prop, fullPath, value);
+        return setValue;
+      },
+    };
+  }
+  function observe(item: GenericObserved, hand: object, path: string = "") {
+    const keys = Object.keys(item);
+    keys.forEach((key) => {
+      if (typeof item[key] === "object") {
+        item[key] = observe(item[key], hand, path + key);
+      }
+    });
+    return new Proxy(item, pathedHandler(path));
+  }
+  return observe(observed, pathedHandler());
 }
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,34 +1,80 @@
-import iObject from '../src/index';
+import iObject from "../src/index";
 
-describe('iObject', ()=>{
-    let logger;
-    beforeEach(()=>{
-        logger = jest.spyOn(console, 'log');
-    })
-    test('reports when new field added', ()=>{
-        const objected = iObject({});
-        objected.foo = 1;
-        expect(logger).toBeCalledWith('Set: /foo to 1')
-    })
-    test('reports when editing existing field', ()=>{
-        const objected = iObject({foo: 2});
-        objected.foo = 1;
-        expect(logger).toBeCalledWith('Set: /foo to 1')
-    })
-    test('reports when editing nested field', ()=>{
-        const objected = iObject({foo: {bar: 2}});
+describe("iObject", () => {
+  let logger;
+  beforeEach(() => {
+    logger = jest.spyOn(console, "log");
+  });
+  test("reports when new field added", () => {
+    const objected = iObject({});
+    objected.foo = 1;
+    expect(logger).toBeCalledWith("Set: /foo to 1");
+  });
+  test("reports when editing existing field", () => {
+    const objected = iObject({ foo: 2 });
+    objected.foo = 1;
+    expect(logger).toBeCalledWith("Set: /foo to 1");
+  });
+  test("reports when editing nested field", () => {
+    const objected = iObject({ foo: { bar: 2 } });
+    objected.foo.bar = 1;
+    expect(logger).toBeCalledWith("Set: foo/bar to 1");
+  });
+  test("reports when editing nested field a second time", () => {
+    const objected = iObject({ foo: { bar: 2 } });
+    objected.foo.bar = 1;
+    objected.foo.bar = 3;
+    expect(logger).toBeCalledWith("Set: foo/bar to 3");
+  });
+  test.skip("reports when replacing object", () => {
+    let objected = iObject({ foo: 2 });
+    objected = { foo: 2 };
+    expect(logger).toBeCalledWith("edited");
+  });
+  describe("options", () => {
+    describe("setValue", () => {
+      test("sets value if not set", () => {
+        const objected = iObject({ foo: { bar: 2 } });
         objected.foo.bar = 1;
-        expect(logger).toBeCalledWith('Set: foo/bar to 1')
-    })
-    test('reports when editing nested field a second time', ()=>{
-        const objected = iObject({foo: {bar: 2}});
+        expect(objected.foo.bar).toEqual(1);
+      });
+      test("sets value if true", () => {
+        const objected = iObject({ foo: { bar: 2 } }, { setValue: true });
         objected.foo.bar = 1;
-        objected.foo.bar = 3;
-        expect(logger).toBeCalledWith('Set: foo/bar to 3')
-    })
-    test.skip('reports when replacing object', ()=>{
-        let objected = iObject({foo: 2});
-        objected = {foo: 2}
-        expect(logger).toBeCalledWith('edited')
-    })
-})
+        expect(objected.foo.bar).toEqual(1);
+      });
+      test("doesn't set value if false", () => {
+        let objected;
+        const t = () => {
+          objected = iObject({ foo: { bar: 2 } }, { setValue: false });
+          objected.foo.bar = 1;
+        };
+        expect(t).toThrow(
+          "'set' on proxy: trap returned falsish for property 'bar'"
+        );
+        expect(objected.foo.bar).toEqual(2);
+      });
+    });
+    describe("reporter", () => {
+      test("uses console.log as default", () => {
+        const objected = iObject({ foo: { bar: 2 } });
+        objected.foo.bar = 1;
+        expect(logger).toBeCalledWith("Set: foo/bar to 1");
+      });
+      test("allows override", () => {
+        const mockReporter = jest.fn();
+        const objected = iObject(
+          { foo: { bar: 2 } },
+          { reporter: mockReporter }
+        );
+        objected.foo.bar = 1;
+        expect(mockReporter).toBeCalledWith(
+          { foo: { bar: 1 } },
+          "bar",
+          "foo/bar",
+          1
+        );
+      });
+    });
+  });
+});
